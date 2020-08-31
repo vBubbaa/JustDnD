3ffbbd209e2313c26f52cc48da1b73a98731de39
vBubbaa <itylerjames@gmail.com>
2020-08-31 15:49:46 -0700

    Obtain token custom view setup

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework.authtoken import views
+from authentication.views import CustomAuthToken
 
 urlpatterns = [
+    path('obtaintoken/', CustomAuthToken.as_view())
 ]
