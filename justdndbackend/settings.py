69f313254a88153f47332be9b08cd59ee445fcde
vBubbaa <itylerjames@gmail.com>
2020-09-24 12:08:55 -0700

    Add server ip to allowed hosts

--- justdndbackend/settings.py
+++ justdndbackend/settings.py
@@ -23,7 +23,8 @@ if os.environ.get('justrolldnd_env') == 'dev':
     CORS_ORIGIN_ALLOW_ALL = True
 else:
     DEBUG = False
-    ALLOWED_HOSTS = ['justrolldnd.com']
+    ALLOWED_HOSTS = ['justrolldnd.com',
+                     'http://138.68.2.63/', 'https://138.68.2.63/']
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
