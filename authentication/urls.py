79f2454407e15b4f8ba721452513d7b1ac879b93
vBubbaa <itylerjames@gmail.com>
2020-09-24 17:18:26 -0700

    remove session auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from rest_framework.authtoken import views
 from authentication.views import CustomAuthToken
 from authentication import views
-from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('obtaintoken/', csrf_exempt(CustomAuthToken.as_view())),
+    path('obtaintoken/', CustomAuthToken.as_view()),
     path('userdata/', views.UserRetrieveUpdateDestroyAPIView.as_view()),
     path('fetchuseroverview/<slug:user>/',
          views.FetchUserOverview.as_view()),
-    path('register/', csrf_exempt(views.CreateUser.as_view())),
+    path('register/', views.CreateUser.as_view()),
 ]
--- justdndbackend/settings.py
+++ justdndbackend/settings.py
@@ -55,8 +55,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.TokenAuthentication'
     ),
 }
 
